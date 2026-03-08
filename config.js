347bd74fd1b8978a4d521750adbaaecbd5133985
felsec01 <felipesecundino@gmail.com>
2026-03-08 01:08:46 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+// config.js
+window.CLEAN_HELMET_CONFIG = {
+  firebase: {
+    apiKey: "AIzaSyCO3ilDnLT2RjnFpzrIRBG1jxMrDppmEIA",
+    authDomain: "cleanhelmet-e55b7.firebaseapp.com",
+    databaseURL: "https://cleanhelmet-e55b7-default-rtdb.firebaseio.com",
+    projectId: "cleanhelmet-e55b7",
+    storageBucket: "cleanhelmet-e55b7.firebasestorage.app",
+    messagingSenderId: "862264948080",
+    appId: "1:862264948080:web:c45791659355e509634bb5"
+  },
+  backend: {
+    baseUrl: window.location.origin,
+    apiPath: '/api',
+    websocketEnabled: true
+  }
+};
